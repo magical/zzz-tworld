543991a965758023ac3dc56016d4761e6bc973f2
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2009-11-18 03:52:10 -0800

    Slight tweak to tworld.sh

--- tworld.sh
+++ tworld.sh
@@ -1,2 +1,2 @@
-tworld () { make tworld.exe && { { HOME= ./tworld "$@" & }; pid=$! ; cat stdout.txt; cat stderr.txt && tail -n 0 -f stderr.txt --pid=$pid ; } ; }
+tworld () { make tworld.exe && { { HOME= ./tworld "$@" & }; pid=$! ; cat stdout.txt; cat stderr.txt ; tail -n 0 -f stderr.txt --pid=$pid ; } ; }
 
